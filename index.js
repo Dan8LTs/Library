51de812a81e4d6d8c581ae1f1d361b6b8eeb465a
Danil Maskin <57964342+Dan8LTs@users.noreply.github.com>
2024-02-21 23:46:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+
+button.addEventListener('click', function() {
+	output.innerHTML = "Мужчина оказался единственным выжившим в шторме. Он попал на необитаемый остров, где провёл большую часть жизни в одиночестве, борясь за выживание. Спустя 26 лет ему удалось вернуться на родину.";
+});
